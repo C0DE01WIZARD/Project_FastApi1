bc695e4f448246a05a2496595fff2ecf8930f391
Rushan <usmanov-rushan@mail.ru>
2023-11-26 14:49:16 +0300

    Commit v 0.1

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def function():
+    a = "Hello, Python"
+    return {"message": a}
+
+
+@app.post("/post")
+async def function():
+    a = "Hello, Python"
+    return {"message": a}
